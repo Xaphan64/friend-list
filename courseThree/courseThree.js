45836a5ba6ecb9acbcb22cf9bbb81b69bfbf73ef
Xaphan64 <alex_daniel64@yahoo.com>
2026-04-04 17:24:57 +0300

    added dot status colors

--- courseThree/courseThree.js
+++ courseThree/courseThree.js
@@ -3,6 +3,7 @@ async function loadData() {
   let response = await fetch("friends.json");
   let data = await response.json();
 
+  // render the all friends
   handleRender(data.friends);
 }
 
@@ -13,23 +14,53 @@ function handleRender(friends) {
   // define friend elements in html
   friends.forEach((friend) => {
     friendList += `
-     <div id="friend">
-        <img src="${`${friend.icon}?img=${Math.floor(Math.random() * 70)}`}" alt="${friend.name}" />
-        <div id="nameContainer">
-          <p id="friendName">${friend.name}</p>
-          <p id="friendNickname">${friend.nickname}</p>
+      <div class="friend">
+        <div class="imageContainer">
+            <img src="${`${friend.icon}${friend.name}`}" alt="${friend.name}" />
+            <svg height="20" width="20" xmlns="http://www.w3.org/2000/svg">
+              <circle r="10" cx="10" cy="10" fill="${handleDotColor(friend.status)}" />
+            </svg>
         </div>
-        <div id="friendStatus">${friend.status}</div>
-        <div id="friendDate">${friend.birthDate}</div>
+
+        <div class="textContainer">
+            <div class="nameContainer">
+                <p class="friendName">${friend.name}</p>
+                <p class="friendNickname">${friend.nickname.length == 0 ? "" : `(${friend.nickname})`}</p>
+            </div>
+
+            <div class="friendStatus">${friend.status}</div>
+
+            <div class="friendDate">${friend.birthDate}</div>
+        </div>
+        
+        <button type="button" class="moreOptions" onclick="handleFriendMenu()">
+          &#8801
+        </button>
       </div>
     `;
   });
 
   // get the html friends container
-  const friendsContainer = document.getElementById("friendsContainer");
+  const friendsContainer = document.querySelector(".friendsContainer");
 
   // insert the friendlist into the html friendsContainer
   friendsContainer.innerHTML = friendList;
+}
+
+function handleFriendMenu() {
+  console.log("friend menu clicked");
+}
+
+// handle the status dot color based on the status
+function handleDotColor(status) {
+  if (status === "online") {
+    return "#41ff00";
+  } else if (status === "away") {
+    return "#ffbc00";
+  } else if (status === "busy") {
+    return "#ff0000";
+  }
+  return "gray";
 }
 
 loadData();
